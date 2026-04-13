89bab4d17c8a686f02b28fe85b726cbd9bf48e36
PhSM-PhEN <paulohenrique.phen@hotmail.com>
2026-04-13 00:14:31 -0300

    adcionando novos usecase

--- src/FleetManager.Application/UseCase/ToVehicle/GetAll/GetAllVehicleUseCase.cs
+++ src/FleetManager.Application/UseCase/ToVehicle/GetAll/GetAllVehicleUseCase.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using FleetManager.communication.Resposnes;
 using FleetManager.Domain.Repositories.ToVehicle;
-using FleetManager.Infrastructure.DataAccess;
-using Microsoft.EntityFrameworkCore;
 
 namespace FleetManager.Application.UseCase.ToVehicle.GetAll
 {
@@ -10,19 +8,11 @@ namespace FleetManager.Application.UseCase.ToVehicle.GetAll
     {
         private readonly IMapper _mapper = mapper;
         private readonly IVehicleReadOnlyRepository _vehicleReadOnly = vehicleReadOnly;
-        private readonly FleetManagerDbContext _dbContext;
+     
         public async Task<ResponseAllVehicleJson> Execute()
         {
             var vehicles = await _vehicleReadOnly.GetAll();
-            _dbContext.Vehicles.AsNoTracking().Select(v => new ResponseVehicleJson
-            {
-                Id = v.Id,
-                LicensePlate = v.LicensePlate,
-                
-                Brand = v.Brand,
-                FuelType = v.FuelType.ToString(),
-                TransmissionType = v.TransmissionType,
-            }).ToListAsync();
+            
 
             return new ResponseAllVehicleJson
             {
