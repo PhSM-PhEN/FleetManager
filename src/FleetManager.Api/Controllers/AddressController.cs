245944f5c73cc7d49383b0679bb7fd113a23f371
Paulo Mota <paulohenrique.phsm@hotmail.com>
2026-04-29 10:31:06 -0400

    refatoring controller address

--- src/FleetManager.Api/Controllers/AddressController.cs
+++ src/FleetManager.Api/Controllers/AddressController.cs
@@ -1,6 +1,7 @@
 using FleetManager.Application.UseCase.ToAddress.Register;
 using FleetManager.communication.Requests.ToAddress;
 using FleetManager.communication.Resposnes;
+using FleetManager.communication.Resposnes.ToAddress;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FleetManager.Api.Controllers
@@ -10,7 +11,7 @@ namespace FleetManager.Api.Controllers
     public class AddressController : ControllerBase
     {
         [HttpPost]
-        [ProducesResponseType(typeof(RequestAddressJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseAddressJson), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Register([FromServices] IRequestRegisterAdressUseCase useCase,
          [FromBody] RequestAddressJson request )
