63bc63fe28b5d9ce9d8cdc472863d40bdfff601f
PhSM-PhEN <paulohenrique.phen@hotmail.com>
2026-04-28 21:09:08 -0300

    finalize user usecase

---
+++ src/FleetManager.Application/UseCase/ToUser/ChangePassword/IChangePasswordUserUseCase.cs
@@ -0,0 +1,9 @@
+﻿using FleetManager.communication.Requests.ToUser;
+
+namespace FleetManager.Application.UseCase.ToUser.ChangePassword
+{
+    public interface IChangePasswordUserUseCase
+    {
+        Task Execute(RequestChangePasswordJson request);
+    }
+}
