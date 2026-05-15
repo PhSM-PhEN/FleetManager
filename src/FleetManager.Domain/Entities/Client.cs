89665182d9a9be0739080981d4577141849bd70e
Paulo Mota <paulohenrique.phsm@hotmail.com>
2026-05-15 17:34:43 -0400

    ASD

--- src/FleetManager.Domain/Entities/Client.cs
+++ src/FleetManager.Domain/Entities/Client.cs
@@ -6,11 +6,12 @@ public class Client
 {
     public long Id {get; set;}
     public string FirstAndLastName {get; set; } = string.Empty;
+    public string PhoneNumber {get ; set ;} = string.Empty;
     public string RG {get ; set ;} = string.Empty;
     public string CPF {get; set;} = string.Empty;
     public string CnhRegisterNumber {get; set;} = string.Empty;
     public string CnhCategory {get; set;} = string.Empty;
-    
+    public long AddressId {get; set;}
     public Address Address {get; set; } = default!;
 
 }
